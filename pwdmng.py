6fb6ae63beb6e4ca733dbd5626d684d125ff0b6e
Amritesh Ganiger <51706219+amriteshganiger@users.noreply.github.com>
2019-09-05 12:57:30 +0530

    Working on Command Line

--- pwdmng.py
+++ pwdmng.py
@@ -1,3 +1,15 @@
+#!/usr/bin/python3
+
+#Importing GUI library Tkinter
+import tkinter as ttk 
+#Importing MySQL Lib
+import mysql.connector as mysql
+
+#Connecting to Database
+db=mysql.connect(host="localhost",user="#MYSQLUSER",password="#MYSQLPASS",database="#MYSQLDATABASE")
+
+#Creating Cursor object to take actions through script
+cursor=db.cursor()
 
 pin=input("Please enter the Pin to ENTER: ")
 
@@ -16,5 +28,45 @@ while pin == "7580":
         break
     else: 
         print("You have selected wrong option!")
-        
 
+def commandExe(command):
+    cursor.execute(command)
+
+
+def manager():
+    while pin == "7580":
+        option=input("""\nWould you like to ADD or FETCH?
+                        ADD = 1 
+                        FETCH = 2
+                        quit=Q: """)
+        if option == '1':
+            nickname=input("Enter a nickname for this save: ")
+            usrname=input("Enter username for this save: ")
+            email=input("Enter your email: ")
+            pwd=input("Enter your password: ")
+            add="INSERT INTO users(NICKNAME,USERNAME,EMAIL,PASS) VALUES ('%s','%s','%s','%s')"%(nickname,usrname,email,pwd)
+            commandExe(add)
+            db.commit()
+            continue
+        elif option=='2':
+            output=cursor.fetchall()
+            print ("Select your Nickname ID: \n")
+            for x in output:
+                print (x[0], ":" ,x[2])
+            fetchID=int(input("\n ENTER ID: ") )  
+
+            print ("""\nThis is your USERNAME: {}
+This is your EMAIL: {}
+This is your PASSWORD: {}  """.format( output[fetchID-1][1],output[fetchID-1][3],output[fetchID-1][4] ))         
+        elif option=="Q" or "q":
+            break
+        else: 
+            print("You have selected wrong option!")
+
+#Fetching all data from DB
+fetch="SELECT * FROM users"
+commandExe(fetch)
+
+manager()
+
+db.close()
